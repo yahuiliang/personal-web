cbf0f0f1f0e190e60f2cca71877ce28c9cf4a517
liangy <liangy@msoe.edu>
2019-02-03 15:57:29 -0600

    save for connecting with the repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MenuBar from './components/nav-bar/nav-bar';
+import Home from './pages/home/home';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {tab: "home"}
+  }
+
+  renderTab() {
+    let tab = undefined;
+    switch (this.state.tab) {
+      case "home":
+        tab = <Home/>;
+        break;
+      case "about":
+        tab = <div>about</div>;
+        break;
+      case "contact":
+        tab = <div>contact</div>;
+        break;
+      default:
+        break;
+    }
+    return tab;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MenuBar itemSelected={(category) => {this.setState({tab: category})}}/>
+        {this.renderTab()}
       </div>
     );
   }
---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './nav-bar.css';
+
+class MenuBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+          <a className="navbar-brand mb-0 h1"
+             href={"#Home"}
+             onClick={() => {
+               this.props.itemSelected("home")
+             }}>
+            Yahui.com
+          </a>
+          <button className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarText"
+                  aria-controls="navbarText"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href={"#Home"} onClick={() => {
+                  this.props.itemSelected("home")
+                }}>
+                  Home
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href={"#About"}
+                   onClick={() => {
+                     this.props.itemSelected("about")
+                   }}>
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link"
+                   href={"#Projects"}
+                   onClick={() => {
+                     this.props.itemSelected("contact")
+                   }}>
+                  Projects
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default MenuBar;
