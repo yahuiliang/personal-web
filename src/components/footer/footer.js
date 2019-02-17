bcd9ec82e0455f9076abd92053e0a633dd4dfc36
liangy <liangy@msoe.edu>
2019-02-16 21:08:46 -0600

    finished implementing the skeleton of navbar and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,16 +4,17 @@ import './footer.css'
 class Footer extends React.Component {
   render() {
     return (
-      <footer id="footer">
-        {/* <footer> */}
-        <div id="copyright">
-          &copy; Yahui Liang Website 2019
+      <footer>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <div id="container">
+          <div id="copyright">
+            &copy; Yahui Liang Website 2019
+          </div>
+          <div id="external-links">
+            <a href="https://www.linkedin.com/in/yahui-liang/" class="fa fa-linkedin fa-lg"></a>
+            <a href="https://github.com/yahuiliang" class="fa fa-github fa-lg"></a>
+          </div>
         </div>
-        <div id="external-links">
-          <a href="https://github.com/yahuiliang">Github</a>
-          <a href="https://www.linkedin.com/in/yahui-liang/">LinkedIn</a>
-        </div>
-        {/* </footer> */}
       </footer>
     )
   }
