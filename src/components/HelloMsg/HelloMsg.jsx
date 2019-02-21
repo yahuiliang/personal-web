dd7acf6a1ef8a08395e0d1bc4cdd8c9bc067c652
liangy <liangy@msoe.edu>
2019-02-21 15:18:50 -0600

    Finished implementing the hello message

---
+++ src/components/HelloMsg/HelloMsg.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../../index.css'
+import './HelloMsg.css';
+
+class HelloMsg extends Component {
+  render() {
+    return (
+      <div id="hello-msg" height="300" width="100">
+        <h5 id="job">I am a software engineer,</h5>
+        <h1 id="name">
+          <b>Yahui Liang</b>
+        </h1>
+        <div id="meaningful-words">
+          <p id="text">“If I have seen further than others, it is by standing upon the shoulders of giants.”</p>
+          <p id="author">-Sir Isaac Newton</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HelloMsg;
